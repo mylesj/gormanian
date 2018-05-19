047e0156d226f2f16968f800d678794687a1810f
Myles J <hi@myles.ninja>
2018-05-19 16:42:32 +0100

    chore: ordinal date off by one

--- src/util/ordinal-date.js
+++ src/util/ordinal-date.js
@@ -1,6 +1,6 @@
 const fromOrdinalDate = calculate => ord =>
 	isNaN(ord) ? NaN : ord > 364 ? -1 : calculate(ord)
 
-export const toMonth = fromOrdinalDate(ord => Math.floor(ord / 28))
-export const toDate = fromOrdinalDate(ord => ord % 28)
-export const toDay = fromOrdinalDate(ord => ord % 7)
+export const toMonth = fromOrdinalDate(ord => Math.floor((ord - 1) / 28))
+export const toDate = fromOrdinalDate(ord => (ord - 1) % 28 + 1)
+export const toDay = fromOrdinalDate(ord => (ord - 1) % 7)
